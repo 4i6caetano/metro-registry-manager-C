31a7cb315eb0075afa61e527fccb43750c5fa084
4i6 <joaopcaetanoc@gmail.com>
2026-07-01 23:36:24 -0300

    docs: initiates the function.

--- functions/functionsGraph.c
+++ functions/functionsGraph.c
@@ -2,9 +2,29 @@
 
 #include "functionsGraph.h"
 
+/*
+A representação do grafo deve ser, obrigatoriamente, na forma de listas de
+adjacências. As listas de adjacências consistem tradicionalmente em um vetor de |V|
+elementos que são capazes de apontar, cada um, para uma lista linear, de forma que o
+i-ésimo elemento do vetor aponta para a lista linear de arestas que são adjacentes ao
+vértice i. Cada elemento do vetor deve representar o nome de uma estação. Um
+exemplo de nome de estação é “Luz”. Os vértices do vetor devem ser ordenados de
+forma crescente de acordo com o nome da estação. Note que, segundo a especificação
+do trabalho prático, se duas ou mais estações têm o mesmo nome, elas são
+consideradas a mesma estação.
+Cada elemento da lista linear representa uma aresta entre duas estações e deve
+armazenar: (i) o nome da próxima estação; (ii) a distância para a próxima estação; e
+(iii) os nomes de linha associados.
+*/
+
 int createGraphFromMetro(FILE* binaryMetroFile)
 {
-
+  // criar a lista de adjacencia
+  // Ou seja, iniciar a lista
+  // criar os vértices
+  //criar as conexões
+  // Ir preenchendo ela com cada vertice (estacao que temos)
+  // preencher cada conexão do vertice com as informações
 }
 
 int calculateLowestDistanceFromStations(FILE* binaryMetroFile, char* originStationName, char* valueOriginStation, char* destinyStationName, char* value)
